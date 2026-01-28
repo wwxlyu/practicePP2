0096859ebe80b09437da1cd2c4cd57f7bc5ad217
wwxlyu <a9216996@gmail.com>
2026-01-28 11:23:58 +0500

    New changes

--- python-basics/lab1a.py
+++ python-basics/lab1a.py
@@ -1,2 +1,44 @@
+#1 Data types
+x = 35e3
+y = 12E4
+z = -87.7e100
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#2
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+#convert from int to float:
+a = float(x)
+
+#convert from float to int:
+b = int(y)
+
+#convert from int to complex:
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+#3
+a = "Hello, World!" #otput 1 index
+print(a[1])
 
 
+#4
+a = "Let's make it better!"
+print(len(a))
+
+
+#5
+txt = "I would like to have more free time!"
+print("free" in txt)
---
+++ python-basics/lab1b.py
@@ -0,0 +1,38 @@
+#1
+a =10
+b= 5
+if a>b:
+   print("Right")
+
+#2
+c = 200
+d = 33
+if d > c:
+  print("d is greater than c")
+else:
+  print("d is not greater than c")
+
+#3
+x = "Hello"
+y = 15
+
+print(bool(x))
+print(bool(y))
+
+#4
+print(bool(False))
+print(bool(None))
+print(bool(0))
+print(bool(""))
+print(bool(()))
+print(bool([]))
+print(bool({}))
+
+#5
+p=6
+d=2
+result=6/2
+if result %2 == 0 :
+   print('Thats True')
+else:
+   print('Thats False')
