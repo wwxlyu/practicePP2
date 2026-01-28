f86228f6d88c639f6df763a9f21148780d032174
wwxlyu <a9216996@gmail.com>
2026-01-28 11:02:26 +0500

    Added changes

--- python-basics/lab1.py
+++ python-basics/lab1.py
@@ -1,3 +1,28 @@
+#1 (Variables)
+x=5
+y='Miya'
+print(x)
+print(y)
 
+#2
+v=str(5)
+w=int(5)
+z=float(5)
+print(v, w, z)
 
+#3
+f=67
+p="Hey"
+print(type(f))
+print(type(p))
 
+#4
+c="John"
+d='John'
+print(type(c))
+print(type(d))
+
+#5 upper case and lower case isnt the same and its not overwrite
+a=4
+A="Four"
+print(a, A)
