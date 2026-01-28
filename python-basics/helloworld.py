4c93ee37fcd88aef5da9157e7f5e24202effc09e
wwxlyu <a9216996@gmail.com>
2026-01-28 10:46:25 +0500

    More codes were added

--- python-basics/helloworld.py
+++ python-basics/helloworld.py
@@ -1,1 +1,20 @@
-print("Hello world!")
+#1
+print("Hello world!") #output string
+
+#2
+x=5
+x=3
+print(x)
+
+#3
+d=23
+d='F'
+print(d)
+
+#4
+a="Hello"
+a=67
+print(a)
+
+#5
+print("I am", 19, "years old")
--- python-basics/lab1.py
+++ python-basics/lab1.py
@@ -1,12 +1,3 @@
-x=5
-x=3
-print(x)
 
-d=23
-d='F'
-print(d)
 
-a="Hello"
-a=67
-print(a)
 
