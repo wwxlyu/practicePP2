1925f1ee7300e9eb49785c7a77691a19c0cf3388
wwxlyu <a9216996@gmail.com>
2026-03-18 12:39:32 +0500

    upd

--- Practice6/file_handling/read_files.py
+++ Practice6/file_handling/read_files.py
@@ -1,3 +1,4 @@
 with open("example.txt", "r") as f:
     content = f.read()
     print(content)
+
--- Practice6/file_handling/write_files.py
+++ Practice6/file_handling/write_files.py
@@ -1,3 +1,6 @@
 with open("example.txt", "w") as f:
     f.write("Good afternoon\n")
     f.write("If you read this - everything is working\n")
+
+with open("new.txt", "x") as f:
+    f.write("New file")
